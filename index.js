f8f4b912d35309d7da51d79f1865d9fb3b6a6f01
Micael Rauan <87452033+micaelrauan@users.noreply.github.com>
2025-11-23 16:00:18 -0300

    Testando servidor com sessões

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 30, // -30dias tempo de login maximo
     },
     store: sessionStore,
